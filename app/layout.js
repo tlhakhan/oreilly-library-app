bbed74a2c0342fbee13c05d073d16b855b96d707
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2026-03-30 00:11:40 +0000

    Add prettier

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata = {
   title: "O'Reilly Library Explorer",
   description: "Quickly find and explore books",
-};
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -23,12 +23,10 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>
-      <div className="h-screen flex flex-col p-6">
-      <div className="flex-1 overflow-y-auto">
-        {children}
-      </div>
-      </div>
+        <div className="flex flex-col p-6">
+          <div className="overflow-y-auto">{children}</div>
+        </div>
       </body>
     </html>
-  );
+  )
 }
--- app/publishers/page.js
+++ app/publishers/page.js
@@ -1,28 +1,39 @@
-import {publishers, getBookCount} from '@/db'
-import Link from 'next/link'
+import { publishers, getBookCount } from "@/db"
+import Link from "next/link"
 
-const Publisher = ({publisher: p}) => {
-  return (<div>
-    <span className="pr-1"><Link href={`/publishers/${p.uuid}/books`}>{p.name}</Link></span>
-    <span className="text-sm text-gray-500">({getBookCount(p)})</span>
-  </div>)
+const Publisher = ({ publisher: p }) => {
+  return (
+    <div>
+      <span className="pr-1">
+        <Link
+          className="hover:underline visited:text-purple-600"
+          href={`/publishers/${p.uuid}/books`}
+        >
+          {p.name}
+        </Link>
+      </span>
+      <span className="text-sm text-gray-500">({getBookCount(p)})</span>
+    </div>
+  )
 }
 
 const selected = publishers
-                  .filter(p => p.is_active)
-                  .filter(p => getBookCount(p) > 0)
-                  .map(p => <Publisher key={p.uuid} publisher={p}/>)
+  .filter((p) => p.is_active)
+  .filter((p) => getBookCount(p) > 0)
+  .map((p) => <Publisher key={p.uuid} publisher={p} />)
 
 export default function Page() {
-  return (<div>
-    <header className="flex justify-between items-center border-b shrink-0 border-gray-400">
-      <div>
-        <span className="text-lg font-medium pr-1">Publishers</span>
-        <span className="text-sm text-gray-500">({selected.length})</span>
+  return (
+    <div>
+      <header className="flex justify-between items-center border-b shrink-0 border-gray-400">
+        <div>
+          <span className="text-lg font-medium pr-1">Publishers</span>
+          <span className="text-sm text-gray-500">({selected.length})</span>
+        </div>
+      </header>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-x-4 mt-4">
+        {selected}
       </div>
-    </header>
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-x-4 mt-4">
-      {selected}
     </div>
-    </div>)
+  )
 }
